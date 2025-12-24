46d0a0e45147a433aaa8dceef631442a5de138db
baobabprince <46530629+baobabprince@users.noreply.github.com>
2025-12-24 09:57:39 +0200

    Delete dd.sh

--- dd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-base_url="https://assets.yadvashem.org/image/upload/t_f_low_image/f_auto/v1/remote_media/documentation4/16/12612299_03263622/"
-
-for i in {1..700}
-do
-    wget "${base_url}$(printf "%05d" $i).JPG"
-done
